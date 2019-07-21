614db75d58295424a33f10cb6ef9b963497dccb7
E57CAA91720BA5CCA1E5A6C8BB3FDC34 <E57CAA91720BA5CCA1E5A6C8BB3FDC34@i-search.com.cn>
2019-07-21 11:48:42 +0800

    fix some bugs about create topic

--- src/actions/topic.ts
+++ src/actions/topic.ts
@@ -4,16 +4,18 @@ export class TopicAction{
 
     public async createTopic(page:Page,tab:string,title:string,content:string){
 
-        const tabOption = await page.waitForSelector('#tab-value');
-        await tabOption.click();
+        await page.select('#tab-value',tab);
 
-        // 根据tab文本定位元素
-        const allOption = await page.$$('#tab-value > option');
-        const allOptionVal = await page.$$eval('#tab-value > option',eles=>(eles.map(ele=>ele.textContent)));
-        const index = allOptionVal.findIndex(t=>t==tab);
-        if(index>0){
-            await allOption[index].click();
-        }
+        // const tabOption = await page.waitForSelector('#tab-value');
+        // await tabOption.click();
+
+        // // 根据tab文本定位元素
+        // const allOption = await page.$$('#tab-value > option');
+        // const allOptionVal = await page.$$eval('#tab-value > option',eles=>(eles.map(ele=>ele.textContent)));
+        // const index = allOptionVal.findIndex(t=>t==tab);
+        // if(index>0){
+        //     await allOption[index].click();
+        // }
 
         // title
         const titleInput = await page.waitForSelector('#title');
@@ -22,7 +24,8 @@ export class TopicAction{
         // content
         const contentArea = await page.waitFor('div.CodeMirror-scroll');
         await contentArea.click();
-        await contentArea.type(content);
+        // await contentArea.type(content);
+        await page.keyboard.type(content);
 
         // submit
         const submitBtn = await page.waitForSelector('input[type="submit"]')
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { TopicAction } from './actions/topic'
 import {Page,launch} from 'puppeteer';
 
 async function run(){
-    let browser = await launch({headless:false})
+    let browser = await launch({headless:false,slowMo:100})
     let page:Page =await browser.newPage();
     let useraction = new UserAction();
     let topicAction = new TopicAction();
     await useraction.login(page,'user1','123456');
+    await page.waitFor(3*1000);
 
     await page.goto('http://39.107.96.138:3000/topic/create');
-    await topicAction.createTopic(page,'招聘','11111','sssssss');
+    await topicAction.createTopic(page,'ask','11111','sssssss');
 }
 
 run();
