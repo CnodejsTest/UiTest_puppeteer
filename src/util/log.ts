de90fd9af84a84667c39c156c31425a54f09561d
E57CAA91720BA5CCA1E5A6C8BB3FDC34 <E57CAA91720BA5CCA1E5A6C8BB3FDC34@i-search.com.cn>
2019-09-04 19:39:20 +0800

    add log file

---
+++ src/util/log.ts
@@ -0,0 +1,33 @@
+import { Logger, configure, getLogger } from 'log4js'
+
+
+const logconfig = {
+    appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
+    categories: { default: { appenders: ['cheese'], level: 'debug' } }
+}
+
+export class Log {
+    private static Logger: Logger
+
+    public static init() {
+        configure(logconfig);
+        Log.Logger = getLogger('puppeteer')
+    }
+
+
+    public static debug(debug:string){
+        Log.Logger.debug(debug)
+    }
+
+    public static info(info:string){
+        Log.Logger.info(info)
+    }
+
+    public static warn(warn:string){
+        Log.Logger.warn(warn)
+    }
+
+    public static error(error:string){
+        Log.Logger.error(error)
+    }
+}
